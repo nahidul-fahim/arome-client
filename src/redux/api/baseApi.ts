e5319a11d9c4d28b1b6fa19d89278f981917db3a
nahidul-fahim <nahidul7562@gmail.com>
2025-03-02 13:22:51 +0600

    feat: integrate Redux Toolkit with API setup and custom hooks

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,14 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: "https://tech-pulse-server-six.vercel.app/api/v1",
+    credentials: 'include'
+});
+
+export const baseApi = createApi({
+    reducerPath: 'baseApi',
+    baseQuery: baseQuery,
+    refetchOnMountOrArgChange: true,
+    refetchOnFocus: true,
+    endpoints: () => ({}),
+})
